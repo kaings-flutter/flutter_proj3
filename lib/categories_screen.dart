b4ef39440fb4f728dd704a7937cceaf7c26a2a02
willys_tjiawi <willys@huubap.com>
2020-09-10 12:16:47 +0800

    register category_screen as main screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,14 +5,19 @@ import './dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        children: DUMMY_CATEGORIES
-            .map((cat) => CategoryItem(cat.title, cat.color))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deli Meal'),
+      ),
+      body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((cat) => CategoryItem(cat.title, cat.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20)),
+    );
   }
 }
