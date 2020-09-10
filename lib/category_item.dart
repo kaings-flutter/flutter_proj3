23194f929e08a3953079ce4727d2468c3f484701
willys_tjiawi <willys@huubap.com>
2020-09-10 15:26:28 +0800

    navigating to new page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,18 +7,31 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (builderContext) {
+      return CategoryMealsScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(
-          15), // this will improve performance. `const` will inform flutter not to rerender because it is const (won't change)
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.subtitle1,
+    return InkWell(
+      onTap: () {
+        selectCategory(context);
+      },
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(
+            15), // this will improve performance. `const` will inform flutter not to rerender because it is const (won't change)
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [color.withOpacity(0.7), color]),
+            borderRadius: BorderRadius.circular(15)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [color.withOpacity(0.7), color]),
-          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
