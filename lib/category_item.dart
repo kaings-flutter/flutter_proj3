1f9dd4503a6c70a5fcbf2d335df01264ebda9284
willys_tjiawi <willys@huubap.com>
2020-09-10 11:38:05 +0800

    added category_screen & category_item widget

---
+++ lib/category_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(
+          15), // this will improve performance. `const` will inform flutter not to rerender because it is const (won't change)
+      child: Text(title),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [color.withOpacity(0.7), color]),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
