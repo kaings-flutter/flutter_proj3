fb3790db1bbd7a8a515d56ec1a259079fca0a9b1
willys_tjiawi <willys@huubap.com>
2020-09-25 18:19:47 +0800

    added filter switches part1

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  Map<String, bool> filterOptions = {
+    '_glutenFree': false,
+    '_vegetarian': false,
+    '_vegan': false,
+    '_lactoseFree': false
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +23,34 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Filters Screen'),
       ),
       drawer: MainDrawer(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust Your Meal Selection!',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          // `Expanded` will take the whole remaining space
+          Expanded(
+            child: ListView(
+              children: [
+                SwitchListTile(
+                  title: Text('Gluten-Free'),
+                  subtitle: Text('Only include gluten-free meals.'),
+                  value: filterOptions['_glutenFree'],
+                  onChanged: (newValue) {
+                    setState(() {
+                      filterOptions['_glutenFree'] = newValue;
+                    });
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
